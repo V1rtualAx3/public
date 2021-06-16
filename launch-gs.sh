976614d475d4fd857f46d1e50f1764eb3f29fabe
V1rtualAx3 <47578068+V1rtualAx3@users.noreply.github.com>
2021-06-16 10:52:24 +0200

    Create launch-gs.sh

---
+++ launch-gs.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# *Changelog*
+# [1.0] [MLA] [16/06/2021]  : Creation of this script
+
+# ! SCRIPTS VARIABLES ! #
+sName="launch-gs"
+sDesc="Launch the get-started script automaticaly"
+sLang="bash"
+sVers="1.0"
+
+# Variables legend
+# c = color variables (ex : cRed = Red Color)
+# s = scripts variables (ex : command format, date, etc.)
+# v = standard variables (ex : vVar = string)
+
+# *VARIABLES* #
+
+# [c] - Color varaibles
+cRed="\e[31m" # Print string in red
+cGreen="\e[32m" # Print string in green
+cBlue="\e[34m" # Print string in blue
+cYellow="\e[33m" # Print string in yellow
+cNone="\e[39m" # Print string with standard foreground color
+
+# [s] - Scripts variables
+sUser=$(who am i | awk '{print $1}') # Print elevated user
+sDateLog=$(date +'%d/%m/%d %H:%M') # Format date for log : 01/01/10 10:10
+sDateFile=$(date +'%d%m%d') # Format date for file name : 010110
+sLogFile="/var/log/${sName}_${sDateFile}.log"
+
+# [v] - Standard variables
+vPassphrase=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20 ; echo '')
+
+# *FUNCTIONS* #
+
+getPrintOk () {
+    echo -e "[${cGreen}OK${cNone}] $*"
+}
+
+getPrintInfo () {
+    echo -e "[${cBlue}INFO${cNone}] $*"
+}
+
+getPrintError () {
+    echo -e "[${cRed}ERROR${cNone}] $*"
+}
+
+getPrintWarn () {
+    echo -e "[${cYellow}WARNING${cNone}] $*"
+}
+
+getCheckError () {
+    if [ $? == 0 ]; then
+        getPrintOk "The task was successfully completed"
+    else
+        getPrintError "The task could not be completed"
+        exit 1
+    fi
+}
+
+# *SCRIPT* #
+
+getPrintInfo "Install pre-requisite ---------------------------- [PR]"
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh
+
+getPrintInfo "Clone get-started sources ------------------------ [PR]"
+cd /tmp
+gh repo clone V1rtualAx3/get-started
+
+getPrintInfo "Execution of get-started ------------------------- [PR]"
+cd /tmp
+sudo get-started.sh
